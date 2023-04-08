70ccfd502549e6ce88be1597982688376171f78b
KevinJayke <kevin.jayat@gmail.com>
2023-04-08 18:12:04 +0200

    fix: more structured style component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "./style/Theme";
+import { GlobalStyles } from "./style/Global";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: ${(props) => props.theme.colors.primary};
+`;
+
 function App() {
-  return <div>first commit</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Button secondary>Coucou</Button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
