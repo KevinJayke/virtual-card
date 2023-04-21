88aeafca7d09292da14824ea86a9464580f82187
KevinJayke <kevin.jayat@gmail.com>
2023-04-21 21:42:54 +0200

    refactor: got rid of lines

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,26 +1,10 @@
-import styled from "styled-components";
-import Line from "../assets/svg/Line";
-import { theme } from "../style/Theme";
-import svgIcons from "../assets/svg/svgIcons";
 import { BoxContent } from "../style/BoxStyle";
 import { Bold, Italic, SubTitle, Title } from "../style/TextStyle";
-
-const LineBox = styled.div`
-  margin-bottom: 4px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 const Content = () => (
   <BoxContent>
     <Title as={"h1"}>I’m Kévin Jayat</Title>
-    <SubTitle as={"h2"}>
-      <LineBox>
-        <Line shape={svgIcons.lineLeft} color={theme.colors.primary} />
-        Magician &<Line shape={svgIcons.lineRight} color="blue" />
-      </LineBox>
-    </SubTitle>
+    <SubTitle as={"h2"}>Magician &</SubTitle>
     <Italic>
       <Bold as={"em"}>front-end</Bold> developer
     </Italic>
--- src/style/TextStyle.jsx
+++ src/style/TextStyle.jsx
@@ -21,6 +21,26 @@ export const Title = styled(BaseText)`
 export const SubTitle = styled(BaseText)`
   font-style: italic;
   font-weight: 500;
+  position: relative;
+
+  ::before,
+  ::after {
+    content: "";
+    position: absolute;
+    width: 2px;
+    height: 100%;
+    transform: rotate(90deg);
+    background-color: ${(props) =>
+      props.theme.colors.primary}; /* the two lines on the side */
+  }
+
+  ::before {
+    left: 2rem;
+  }
+
+  ::after {
+    right: 2rem;
+  }
 `;
 
 export const Italic = styled(BaseText)`
